96f32271808b656cfde9824a1714c88b0cf6fc69
Matthijs Openneer <matthijs.openneer@nh1816.nl>
2026-05-06 12:43:56 +0200

    fixed an overflow issue

--- resources/js/components/NavMain.vue
+++ resources/js/components/NavMain.vue
@@ -29,7 +29,7 @@ const { isCurrentUrl } = useCurrentUrl();
                 >
                     <Link :href="item.href">
                         <component :is="item.icon" />
-                        <span>{{ item.title }}</span>
+                        <span class="pr-[3px]">{{ item.title }}</span>
                     </Link>
                 </SidebarMenuButton>
             </SidebarMenuItem>
