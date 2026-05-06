6810773c02c1c0b016bd6cad87bdab267f8d008b
Matthijs Openneer <matthijs.openneer@nh1816.nl>
2026-05-06 13:36:59 +0200

    alterd name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta property="og:title" content="Gym">
+        <meta name="twitter:title" content="Gym">
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
